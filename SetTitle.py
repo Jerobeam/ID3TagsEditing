0a2433b8e5d6e3680e631a9bbb5b558a88db6bc4
Sebastian Röhling <se.roehling@gmx.de>
2016-11-24 18:23:21 +0100

    minor changes

--- SetTitle.py
+++ SetTitle.py
@@ -3,6 +3,8 @@
 import eyed3
 import os
 
+# eyed3.log.setLevel("ERROR")
+
 # directory = raw_input("Please provide the filepath to the folder in which the mp3 files should be edited: ")
 
 directory = "C:\Users\Sebastian\Desktop\Musik"
@@ -13,7 +15,6 @@ for dirName, subdirList, files in os.walk(directory, topdown=False):
     for fname in files:
 
         filepath = os.path.join(dirName, fname)
-        print filepath
 
         if fname.lower().endswith('.mp3'):
             mp3 = eyed3.load(filepath)
@@ -24,8 +25,6 @@ for dirName, subdirList, files in os.walk(directory, topdown=False):
         # Try to edit tags if possible (not working for ID3 V 2.2.0)
         if mp3.tag.version != (2, 2, 0):
             title = mp3.tag.title
-            artist = mp3.tag.artist
-            album = mp3.tag.album
 
             # edit title
             if title is None:
@@ -34,6 +33,7 @@ for dirName, subdirList, files in os.walk(directory, topdown=False):
 
         else:
             version22counter += 1
+            print "File with ID3 V 2.2.0 Tags found: " , filepath.decode("Cp1252")
 
 print("-----------")
 print "Amount of MP3 Files with ID3 V 2.2.0: ", version22counter
